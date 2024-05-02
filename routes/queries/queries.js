3206c7489e1f14ae4191c1b12d91f20c2616fba2
n1563871 <Luis.Gonzalez02@LibertyFianzas.com>
2024-05-01 22:47:22 -0600

    queries POST & GET endpoints working

--- routes/queries/queries.js
+++ routes/queries/queries.js
@@ -1,12 +1,29 @@
 import express from "express";
 const queriesRouter = express.Router();
+import fs from 'fs';
 
 queriesRouter.get('/', async (req, res) => {
-    res.status(200).send("GET called");
+    try {
+        const data = fs.readFileSync('queries.json', 'utf8');
+        res.status(200).send(data);
+    } catch (err) {
+        if (res.status)
+            console.error(err);
+        res.status(404).send("queries.json file not found");
+    }
 });
 
 queriesRouter.post('/', async (req, res) => {
-    res.status(200).send("POST called");
+    const queryArray = req.body;
+    const data = JSON.stringify(queryArray, null, 2);
+    try {
+        fs.writeFileSync('queries.json', data);
+        console.log('query array saved to queries.json');
+        res.status(200).send("query array saved");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
 
 export default queriesRouter;
