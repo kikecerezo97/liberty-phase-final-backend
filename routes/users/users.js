b5f813e2bfb3ebb8fcd5352ea808767ed07ba662
n1563871 <Luis.Gonzalez02@LibertyFianzas.com>
2024-05-01 19:56:31 -0600

    users GET endpoint working

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,8 +1,20 @@
 import express from "express";
+import {client, connect} from './db.js';
+const dbName = 'usersdb';
+const collectionName = 'users';
+connect(); // Connect to MongoDB
 const usersRouter = express.Router();
 
 usersRouter.get('/', async (req, res) => {
-    res.status(200).send("GET called");
+    try {
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const prj = {user:1,email:1, _id:0};
+        const users = await collection.find({}).project(prj).toArray();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 usersRouter.post('/', async (req, res) => {
